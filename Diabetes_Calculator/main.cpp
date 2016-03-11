32d80c8cca20b8b87d9b8eeeb8b398d63efa4dee
Ryan Boddy <ryanboddy@ryans-macbook-pro.local>
2016-03-11 13:43:34 -0500

    Create variables that will be later moved to a function which will be called to calculate dosages. This is just the basics for now.

--- Diabetes_Calculator/main.cpp
+++ Diabetes_Calculator/main.cpp
@@ -7,12 +7,35 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+    //declare variables
+    double bloodGlucose;
+    double carbs;
+    double targetBG;
+    double icRatio;
+    double corrFactor;
+    string name;
     
-    //going to try and push this project straight to github from xcode
-    cout <<"Initial commit to prepare for GitHub push";
+    
+    //basic interface for inputs
+    cout <<"Enter your name: ";
+    cin >> name;
+    cout <<"Hello " << name << "! This calculator will help you figure out insulin dosages."
+    " Enter your target blood sugar: ";
+    cin >> targetBG;
+    cout <<"Enter your high blood glucose correction factor: ";
+    cin >> corrFactor;
+    cout <<"How many carbs require one unit of insulin? (Insulin to carb ratio): ";
+    cin >> icRatio;
+    cout <<"What is your current Blood Glucose level: ";
+    cin >> bloodGlucose;
+    cout <<"How many carbs are you eating? Enter 0 if you are not eating: ";
+    cin >> carbs;
+    
+    /*I will be adding if statement to check if user is eating and create a function to call that will calculate dosage, this is just variable bs to get started conceptualizing */
     
 }
